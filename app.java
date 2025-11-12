7c13fbed57b1d03c35b5a26c2caede4bd96c588d
shanyadav7956 <shanyadav7956@gmail.com>
2025-11-12 11:17:09 +0530

    Update app.java

--- app.java
+++ app.java
@@ -4,3 +4,23 @@ public class HelloWorld {
     }
 }
 data added to app.java
+
+//dev-2 push from local
+    import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        // nextInt() reads the next integer from the keyboard
+        int number = reader.nextInt();
+
+        // println() prints the following line to the output screen
+        System.out.println("You entered: " + number);
+    }
+}
